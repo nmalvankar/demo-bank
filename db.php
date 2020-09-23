1bc8ebd6f27d1e2c6b327feda517aef149b61a68
nmalvankar <nmalvankar@gmail.com>
2020-09-23 17:12:52 -0400

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ $username = "user";
 $password = "password";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysql($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
 }
 
 /* Select queries return a resultset */
-if ($result = $conn->query("SELECT@@hostname AS hostname")) {
+if ($result = $conn->query("SELECT @@hostname AS hostname")) {
     $row = $result->fetch_object();
     $result->close();
     printf("DEBUG: Database node => %s\n", $row->hostname);
